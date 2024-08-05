8e785481d8f29c50faa828890f89c18508ea7e92
happyloganli <happyloganli.gmail.com>
2024-08-04 23:17:46 -0700

    remove postgresql

--- sql/queries/job_posts.sql
+++
@@ -1,4 +0,0 @@
--- name: CreateJobPost :one
-INSERT INTO job_posts (id, detail_url, title, company, location, level, apply_url, minimum_qualifications, preferred_qualifications, about_job, responsibilities, created_at)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-    RETURNING *;
